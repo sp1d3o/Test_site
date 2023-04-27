f0617cdd6bbc431f198d6b224442f981529c1e7a
fr13nd <valenta1313@seznam.cz>
2023-04-27 16:46:15 +0200

    Boobs

--- testsite/polls/views.py
+++ testsite/polls/views.py
@@ -3,13 +3,14 @@ from .models import Question, Choice
 from django.views import generic
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.utils import timezone
 
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
 class DetailView(generic.DetailView):
     model = Question
